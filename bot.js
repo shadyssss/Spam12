fa512a127ce051e23700a5a32efb0c8cd9b3fdb7
shadyssss <52051189+shadyssss@users.noreply.github.com>
2019-06-27 14:22:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("593161992894677002")
+let channel =     client.channels.get("593773990758055936")
 setInterval(function() {
 channel.send(`Shady`);
 }, 30)
